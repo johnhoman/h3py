b4f59ce59f974998df9937f69c30bf44ca72f120
John Homan <homan.jack@gmail.com>
2018-07-17 00:29:09 -0400

    simplified unit tests

--- tests/test_geoboundary.py
+++ tests/test_geoboundary.py
@@ -2,24 +2,24 @@ import pytest
 from h3py import GeoBoundary, GeoCoord
 import numpy as np
 
-
-def test_GeoBoundary_init_error_on_int():
+@pytest.fixture
+def coord_values():
     coord_values = zip(np.random.random(5) * np.pi,
                        np.random.random(5) * np.pi)
+    return list(coord_values)
+
+
+def test_GeoBoundary_init_error_on_int(coord_values):
     geo_coords = [GeoCoord(lat, lon) for lat, lon in coord_values] + [5]
     with pytest.raises(TypeError):
         GeoBoundary(geo_coords)
 
-def test_GeoBoundary_init_error_on_float():
-    coord_values = zip(np.random.random(6) * np.pi,
-                       np.random.random(6) * np.pi)
+def test_GeoBoundary_init_error_on_float(coord_values):
     geo_coords = [5.4] + [GeoCoord(lat, lon) for lat, lon in coord_values]
     with pytest.raises(TypeError):
         GeoBoundary(geo_coords)
 
-def test_GeoBoundary_init_error_on_str():
-    coord_values = zip(np.random.random(3) * np.pi,
-                       np.random.random(3) * np.pi)
+def test_GeoBoundary_init_error_on_str(coord_values):
     geo_coords = [GeoCoord(lat, lon) for lat, lon in coord_values]
     geo_coords.insert(4, 'mittens')
 
@@ -33,16 +33,12 @@ def test_GeoBoundary_init_error_overflow():
     with pytest.raises(ValueError):
         GeoBoundary(geo_coords)
 
-def test_GeoBoundary_raises_tuple():
-    coord_values = zip(np.random.random(5) * np.pi,
-                       np.random.random(5) * np.pi)
+def test_GeoBoundary_raises_tuple(coord_values):
     geo_coords = tuple(GeoCoord(lat, lon) for lat, lon in coord_values)
     with pytest.raises(TypeError):
         GeoBoundary(geo_coords)
 
 
-def test_GeoBoundary_raises_generator():
-    coord_values = zip(np.random.random(5) * np.pi,
-                       np.random.random(5) * np.pi)
+def test_GeoBoundary_raises_generator(coord_values):
     with pytest.raises(TypeError):
         GeoBoundary(GeoCoord(lat, lon) for lat, lon in coord_values)
